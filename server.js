a52a4abef468c15b9e7284c0e5c2667dbb72e840
Geek1716 <30744385+Geek1716@users.noreply.github.com>
2018-02-22 22:37:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var configDB = require('./config/database.js');
 var cmd = require('node-cmd');
+var multer = require('multer');	
 
 mongoose.connect(configDB.url);
 app.set('view engine', 'ejs');
+
+
 
 app.use(morgan('dev'));
 app.use(bodyParser());
 app.use(cookieParser());
 
-cmd.run('cd best');
 
-/*cmd.get('python3 -m scripts.label_image \
+
+cmd.get('python3 -m scripts.label_image \
     --graph=tf_files/retrained_graph.pb  \
     --image=x.png',
               function(data, err, stderr) {
@@ -27,9 +30,31 @@ cmd.run('cd best');
                   }
                 }
               );
-*/
+
 
 require('./app/routes')(app);
+
+var Storage = multer.diskStorage({
+     destination: function(req, file, callback) {
+         callback(null, "/best");
+     },
+     filename: function(req, file, callback) {
+         callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+     }
+ });
+
+var upload = multer({
+     storage: Storage
+ }).array("imgUploader", 3);
+
+ app.post("/api/Upload", function(req, res) {
+     upload(req, res, function(err) {
+         if (err) {
+             return res.end("Something went wrong!");
+         }
+         return res.end("File uploaded sucessfully!.");
+     });
+ });
 
 app.listen(8080);
 console.log('server running on port 8080');
